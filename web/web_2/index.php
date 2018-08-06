2f1919d6d872f44cc47eab45dc51706f1f19a1ad
sh1llc0de <35120623+sh1llc0de@users.noreply.github.com>
2018-08-06 09:52:11 +0800

    update web_2

---
+++ web/web_2/index.php
@@ -0,0 +1,22 @@
+<?php
+$miwen="a1zLbgQsCESEIqRLwuQAyMwLyq2L5VwBxqGA3RQAyumZ0tmMvSGM2ZwB4tws";
+
+function encode($str){
+    $_o=strrev($str);
+    // echo $_o;
+        
+    for($_0=0;$_0<strlen($_o);$_0++){
+       
+        $_c=substr($_o,$_0,1);
+        $__=ord($_c)+1;
+        $_c=chr($__);
+        $_=$_.$_c;   
+    } 
+    return str_rot13(strrev(base64_encode($_)));
+}
+
+highlight_file(__FILE__);
+/*
+   逆向加密算法，解密$miwen就是flag
+*/
+?>
