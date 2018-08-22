920db24ac4c326cae2022ac33b3fabc49fdcbf8f
sh1llc0de <35120623+sh1llc0de@users.noreply.github.com>
2018-08-23 04:40:30 +0800

    upload web_7

---
+++ web/web_7/Dockerfile
@@ -0,0 +1,4 @@
+FROM php:5.6-apache
+
+COPY /code /var/www/html/
+COPY /flag /
---
+++ web/web_7/code/flag.php
@@ -0,0 +1,8 @@
+<?php
+$FLAG = file_get_contents("/flag");
+
+function getFlag(){
+    global $FLAG;
+    echo $FLAG;
+}
+?>
